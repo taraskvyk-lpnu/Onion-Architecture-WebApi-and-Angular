5e4cfb1d439aa3da89284f0d26e4b9df0def9ea4
Тарас Квик <Taras.Kvyk.PZ.2022@edu.lpnu.ua>
2024-07-07 19:43:31 +0300

    Added Domain/Core Layer

---
+++ Beckend/BookStore.Domain/Models/Book.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.InteropServices.JavaScript;
+
+namespace BookStore.API.Domain;
+
+public class Book : BaseEntity
+{
+    public string Title { get; set; }
+    public string Description { get; set; }
+    [Range(0, 2024, ErrorMessage = "Year must be between 0 and current year")]
+    public int Year { get; set; }
+    
+    [Range(0, 10000, ErrorMessage = "Year must be between 0 and current year")]
+    public decimal Price { get; set; }
+    public string Language { get; set; }
+    public string Author { get; set; }
+    public string Category { get; set; } = null!;
+    public string ImageUrl { get; set; }
+    
+    [Range(10, 5000)]
+    public int PageCount { get; set; }
+    [Range(0, 500)]
+    public int AvailableCount { get; set; }
+}
